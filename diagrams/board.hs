78c8668c5d7b5666f83aaa45ee8d9a9bb680033f
juy1002 <146571154+juy1002@users.noreply.github.com>
2023-12-03 02:10:43 +0800

    Update board.hs

--- diagrams/board.hs
+++ diagrams/board.hs
@@ -1,1 +1,40 @@
 
+import qualified Data.Map as M
+import Data.List
+import Data.String
+import Data.Char
+
+data Side = Black
+            | White
+            deriving (Eq, Show)            
+
+data Blockstat = Occupied Side
+                | Empty 
+                deriving (Eq, Show)
+
+data Board = Mkboard M.Map (Int, Int) Blockstat, Int
+
+showBlock :: Blockstat -> String
+showBlock b = case b of 
+             White -> " o"
+             Black -> " x"
+
+
+pickRow :: Board -> Int -> Int -> Blockstat
+pickRow (Mkboard board size) m n = board ! (m,n)
+
+
+showRow :: Board -> Int -> String
+showRow Bd@(Mkboard board size) m = show n ++ map showBlock (map (pickRow Bd m) [0..(size-1)]) ++ " " ++ show n
+
+labelRow :: Int -> String
+labelRow size = 
+            if size==1 then 
+            "0"
+            else if (size < 10 && size > 1) then 
+                 labelRow (size-1) ++ " " ++ show (size-1)
+            else 
+                 labelRow (size-1) ++ " " ++ chr (ord 'a' + (size-10))
+showBoard :: Board -> String
+showBoard Bd@(Mkboard board size) =
+    labelRow size ++ (map (showRow Bd) [0..(size-1)]) ++ labelRow size
