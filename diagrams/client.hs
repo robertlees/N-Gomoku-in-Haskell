0dd0e4fab73086e6c7ccdaebe5d4b32711a4feed
juy1002 <146571154+juy1002@users.noreply.github.com>
2023-12-14 07:49:01 +0800

    Update client.hs

--- diagrams/client.hs
+++ diagrams/client.hs
@@ -3,6 +3,7 @@ import Network.Socket.ByteString (send, recv)
 import qualified Data.ByteString.Char8 as B8
 
 import System.Environment (getArgs)
+import GHC (HsDataDefn(dd_kindSig))
 
 main :: IO ()
 main = withSocketsDo $ do
@@ -23,42 +24,56 @@ main = withSocketsDo $ do
 
 msgSender1 :: Socket -> IO ()
 msgSender1 sock = do
-  msg <- B8.getLine
-  send sock msg
-  let ymove = B8.pack( "your move is " ++ B8.unpack msg ++ "\n")
-  B8.putStrLn ymove
-
-  msg0 <- recv sock 1000
-
-  B8.putStrLn msg0
-
-  msg1 <- recv sock 1000
-
-  B8.putStrLn msg1
-
--- accept game signal
--- If (signal of ending, player win/lose) then (end game with UI) else
-  msgSender1 sock
+  msg <- ownMove sock
+  if msg == "0" then do
+                    msg1 <- otherMove sock
+                    if msg1 =="0" then do 
+                                        msgSender1 sock
+                                  else -- add player win/lose with signal 1 or 2, for player1 signal 1 is winning 
+                                        putStrLn "Game ends \n"
+                 else -- add player win/lose with signal 1 or 2, for player1 signal 1 is winning 
+                    putStrLn "Game ends \n"
 
 
 msgSender2 :: Socket -> IO ()
 msgSender2 sock = do
 
-  msg0 <- recv sock 1000
+  msg <- otherMove sock
+  if msg == "0" then do
+                    msg1 <- ownMove sock
+                    if msg1 =="0" then do 
+                                        msgSender2 sock
+                                  else -- add player win/lose with signal 1 or 2, for player2 signal 2 is winning 
+                                        putStrLn "Game ends \n"
+                 else-- add player win/lose with signal 1 or 2, for player2 signal 2 is winning 
+                    putStrLn "Game ends \n"
 
-  B8.putStrLn msg0
+
+
+
+ownMove :: Socket -> IO String
+ownMove sock = do
 
   msg <- B8.getLine
   send sock msg
-
   let ymove = B8.pack( "your move is " ++ B8.unpack msg ++ "\n")
   B8.putStrLn ymove
 
+  msg0 <- recv sock 1000
+  if B8.unpack msg0 == "-1" then do
+                                  putStrLn "invalid move, please retry"
+                                  ownMove sock
+                            else do 
+                                  msg1 <- recv sock 1000 --board after self move received here, change if needed by UI
+                                  B8.putStrLn msg1
+                                  return  (B8.unpack msg0)
 
-  msg1 <- recv sock 1000
 
-  B8.putStrLn msg1
 
--- accept game signal
--- If (signal of ending, player win/lose) then (end game with UI) else
-  msgSender2 sock
+otherMove :: Socket -> IO String
+otherMove sock = do
+                    msg0 <- recv sock 1000
+
+                    msg1 <- recv sock 1000 --board after opponent move received here, change if needed by UI
+                    B8.putStrLn msg1
+                    return  (B8.unpack msg0)
